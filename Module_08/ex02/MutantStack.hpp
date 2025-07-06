0624124db46531d73de0fc68cdec835dc9b71986
amineichou <amineichou2022@gmail.com>
2025-07-06 14:23:48 +0100

    update for ex00 and 01 in Moudule08

--- Module_08/ex02/MutantStack.hpp
+++ Module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: moichou <moichou@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 10:42:55 by moichou           #+#    #+#             */
-/*   Updated: 2025/07/04 17:06:38 by moichou          ###   ########.fr       */
+/*   Updated: 2025/07/04 17:26:14 by moichou          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,43 @@
 #include <iostream>
 
 template <typename T>
-class Mutanstack : public std::stack<T>
+class MutantStack : public std::stack<T>
 {
 public:
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+    MutantStack &operator=(const MutantStack &other)
+    {
+        if (this != &other)
+            std::stack<T>::operator=(other);
+        return *this;
+    }
 
-    typename std::stack<T>::container_type::iterator it;
+    ~MutantStack() {}
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    
+    typename std::stack<T>::container_type::iterator begin()
+    {
+        return this->c.begin();
+    }
+
+    typename std::stack<T>::container_type::iterator end()
+    {
+        return this->c.end();
+    }
+
+    typename std::stack<T>::container_type::const_iterator begin() const
+    {
+        return this->c.begin();
+    }
+
+    typename std::stack<T>::container_type::const_iterator end() const
+    {
+        return this->c.end();
+    }
+    
 };
 
 #endif
